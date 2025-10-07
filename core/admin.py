4bb85edb38b09dce731b402854ef19149e186db9
Tomdieu <ivantomdio@gmail.com>
2025-10-07 06:05:19 +0100

    feat: register Topic and Feedback models in admin interface

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Topic, Feedback
 
-# Register your models here.
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'topic', 'created_at']
+    list_filter = ['topic', 'created_at']
+    search_fields = ['user_name', 'content']
+    readonly_fields = ['created_at']
